644344368f0735b0c9e9380887db9692081074e8
Hossein <hossein.aflaki70@gmail.com>
2024-03-30 16:32:14 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'
+import Home from './home'
+ 
+ 
+ 
+
+function App() {
+ 
+
+  return (
+    <>
+     <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+ 
+
+function Header() {
+  return (
+    <div className="  flex justify-between bg-gray-500 p-2">
+      <h1 className="text-white">Book App</h1>
+      <p className="text-violet-400">Botostart <span className="text-white">| React.js Full Courses</span></p>
+    
+    </div>
+  )
+}
+
+export default Header
